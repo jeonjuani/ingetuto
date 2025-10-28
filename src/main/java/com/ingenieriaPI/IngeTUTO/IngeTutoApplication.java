57f421c3b4b84f8b8246af10cfd93897b2843e6d
jeonjuani <juan.castro17@udea.edu.co>
2025-10-27 21:56:00 -0500

    Inicialización del proyecto
    Spring Initializer Generado: Java 17 con Maven, Lombok, JPA y soporte para PostgreSQL

---
+++ src/main/java/com/ingenieriaPI/IngeTUTO/IngeTutoApplication.java
@@ -0,0 +1,13 @@
+package com.ingenieriaPI.IngeTUTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngeTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngeTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingenieriaPI/IngeTUTO/IngeTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenieriaPI.IngeTUTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngeTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
