1cc88d1f77c7ac045efe6c5251f3913a94a6ac51
davidtzz <133818653+davidtzz@users.noreply.github.com>
2025-11-04 09:35:39 -0500

    Create MateriaRepository.java

---
+++ backend/src/main/java/com/ingenieriaPI/IngeTUTO/repository/MateriaRepository.java
@@ -0,0 +1,7 @@
+package com.ingenieriaPI.IngeTUTO.repository;
+
+import com.ingenieriaPI.IngeTUTO.entity.Materia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MateriaRepository extends JpaRepository<Materia,String> {
+}
